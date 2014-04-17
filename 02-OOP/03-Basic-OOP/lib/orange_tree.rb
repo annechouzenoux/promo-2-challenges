58de943650e33ca286d7bcc5e28e0057477b96ae
annechouzenoux <annechouzenoux@yahoo.fr>
2014-04-17 16:40:23 +0200

    first attempt

--- 02-OOP/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,73 @@
+
+
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+
+  attr_accessor :age, :height, :fruits, :alive
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @alive = true
+  end
+
+  def one_year_passes!
+    ages
+    grows
+    produce_fruits
+  end
+
+  def ages
+    @age += 1 unless dead?
+  end
+
+  def grows
+    if @age.between?(1, 10)
+      @height += 1
+    end
+  end
+
+  def produce_fruits
+    if @age.between?(0, 5)
+      @fruits = 0
+    elsif @age.between?(6, 10)
+      @fruits = 100
+    elsif @age.between?(11, 15)
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def dead?
+    !alive?
+  end
+
+
+  def alive?
+
+    if @age.between?(0, 50)
+      @alive = true
+    end
+    if @age >= 100
+      @alive = false
+    end
+    if @age.between?(51, 99)
+      prob_to_die = rand(5)
+      if prob_to_die == 0
+        @alive = true
+      else
+        @alive = false
+      end
+    end
+    @alive
+
+  end
+
+  def pick_a_fruit!
+    @fruits = @fruits - 1
+  end
+
 end
+
+
--- 02-OOP/04-Public-Interface/lib/account.rb
+++ 02-OOP/04-Public-Interface/lib/account.rb
@@ -1,3 +1,4 @@
+
 # This is how you define your own custom exception classes
 class DepositError < StandardError
 end
@@ -12,47 +13,69 @@ class BankAccount
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the position variable)
 
+  attr_reader :name, :position
+
   MIN_DEPOSIT =  100
 
   def initialize(name, iban, initial_deposit, password)
+
     raise DepositError, "Insufficient deposit" unless initial_deposit > MIN_DEPOSIT
     @password = password
     @transactions = []
     @position = 0
     @name, @iban = name, iban
+    @initial_deposit = initial_deposit
 
     add_transaction(initial_deposit)
   end
 
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
+    @position -= amount
     # TODO: returns a string with a message
+    puts "You've just withdrawn #{amount}€"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    @position += amount
+    puts "You've just deposit #{amount}€"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array !
+      if  args[:password] == @password
+        transaction.to_s
+      elsif args[:password] != @password
+        puts "wrong password"
+      elsif args[:password] == nil
+        puts "no password given !"
+      end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    @iban = @iban[0, 3] + "*********" + [-3, -1]
+
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: Displays the account owner, the hidden iban and the position of the account
+    "#{@name}"
+    "#{@iban}"
+    "#{@position}"
   end
 
   private
 
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
+    @transactions << amount
     # TODO: update the current position (which represents the balance of the account)
+    position += amount
   end
 
 end
