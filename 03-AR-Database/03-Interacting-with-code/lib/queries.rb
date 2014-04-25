bba598af92a43b31d639f500e70908524061e637
annechouzenoux <annechouzenoux@yahoo.fr>
2014-04-25 12:50:37 +0200

    Correction exo AR-03

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -6,20 +6,24 @@ db = SQLite3::Database.new(DATABASE_PATH)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
-  db.execute( "SELECT COUNT (*) FROM #{table_name}" )
+  db.execute( "SELECT COUNT (*) FROM #{table_name}" ).flatten.first
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
-  db.execute( "SELECT name FROM Artist ORDER BY name" )
+  db.execute( "SELECT name FROM Artist ORDER BY name" ).flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
-  db.execute("SELECT name FROM Track WHERE name LIKE '%love'")
+  db.execute("SELECT name FROM Track WHERE name LIKE '%love'").flatten
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
-  db.execute( "SELECT name FROM Track WHERE Milliseconds > min_length * 60000")
+  db.execute( "SELECT name FROM Track WHERE Milliseconds > min_length * 60000").flatten
 end
+
+
+p number_of_rows(db, "Track")
+
