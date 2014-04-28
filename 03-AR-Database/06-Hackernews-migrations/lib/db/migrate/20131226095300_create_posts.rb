583154bd31f2e3cf2f44d91a5d0400a31eda1c56
annechouzenoux <annechouzenoux@yahoo.fr>
2014-04-28 12:25:03 +0200

    First attempt, solved it

--- 03-AR-Database/06-Hackernews-migrations/lib/db/migrate/20131226095300_create_posts.rb
+++ 03-AR-Database/06-Hackernews-migrations/lib/db/migrate/20131226095300_create_posts.rb
@@ -1,5 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def up
     #TODO: your code here to create the Posts table
+    create_table :posts do |p|
+      p.string :name
+      p.string :source_url
+      p.datetime :date
+    end
+
   end
 end
