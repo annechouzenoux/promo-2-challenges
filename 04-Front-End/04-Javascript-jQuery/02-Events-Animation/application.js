2bb686c9723b2ee18aa7f22cec079c3392602d14
annechouzenoux <annechouzenoux@yahoo.fr>
2014-05-05 16:21:44 +0200

    cool

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,14 @@
   * TODO: add your code here!
   *
 */
+
+$("#message").hide().slideDown(1000);
+
+$("#message").click(function(e) {
+  $(this).slideUp("slow");
+  });
+
+//$("#messsage").click(function() {
+  //$(this).mouseover();
+//});
+
